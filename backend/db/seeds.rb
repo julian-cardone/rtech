9673369772878b86b559c09ba03e1ea34f2b1e0a
Julian Cardone <juliancardone1@gmail.com>
2023-07-26 18:31:17 -0400

    db setup. now test routes

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "Destroying tables..."
+
+School.destroy_all
+Book.destroy_all
+
+puts "resetting primary keys..."
+    #so that after seeding, the first `User` has `id` of 1
+%w(schools books).each do |table_name|
+    ApplicationRecord.connection.reset_pk_sequence!(table_name)
+end
+
+puts "creating schools..."
+
+50.times do
+    School.create(name: Faker::University.name)
+end
+
+puts 'creating books...'
+
+500.times do
+    school = School.order('RANDOM()').first # Get a random school from the database
+    Book.create(
+      title: Faker::Book.title,
+      price: Faker::Commerce.price(range: 10..100.0),
+      school_id: school.id
+    )
+end
+
+puts "seeding complete!"
+
+
