a8ff7d0495725c7cba79906776e4216e146360b9
Julian Cardone <juliancardone1@gmail.com>
2023-07-28 19:17:34 -0400

    deploy to render

--- rtech/config/routes.rb
+++ rtech/config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     resources :books, only: [:index, :show]
     resources :schools, only: :index
   end
+
+  get '*path', to: "static_pages#frontend_index"
 
 end
